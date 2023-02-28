fa68200066a178cd40a554bff46cfeb5e4419a68
Vladislav Osin <osinvladislav@yandex.ru>
2023-02-28 18:38:13 +0300

    Some libs added

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -7,13 +7,12 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-
-import 'package:bearcorner_mobile/main.dart';
+import 'package:medvezhiy_ugol/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+    await tester.pumpWidget(const UgolApp());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
