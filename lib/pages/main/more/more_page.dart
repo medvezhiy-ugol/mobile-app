6458ab625489e2e20864955d2fc65bfb76e5ae56
Vladislav Osin <osinvladislav@yandex.ru>
2023-03-02 00:06:38 +0300

    Initial commit

---
+++ lib/pages/main/more/more_page.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:medvezhiy_ugol/utils/more_page_icons.dart';
+
+class MorePage extends StatelessWidget {
+  const MorePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: SingleChildScrollView(
+        child: Column(
+          children: [],
+        ),
+      ),
+    );
+  }
+}
--- lib/utils/app_colors.dart
+++ lib/utils/app_colors.dart
@@ -3,5 +3,6 @@ import 'package:flutter/material.dart';
 class AppColors {
   static const Color colorEFEFEF = Color(0xffEFEFEF);
   static const Color color808080 = Color(0xff808080);
+  static const Color color1C1C1C = Color(0xff1C1C1C);
   static const Color color111216 = Color(0xff111216);
 }
