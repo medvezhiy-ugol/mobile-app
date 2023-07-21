6888c418dbdf2843f6e0847b897edbd6d6e1754c
yarin <yarinmaru@gmail.com>
2023-07-21 21:46:43 +0300

    Акции done

--- lib/pages/more/about_app/history_order.dart
+++ lib/pages/more/about_app/history_order.dart
@@ -30,14 +30,62 @@ class _HistoryOrderState extends State<HistoryOrder> {
                         Navigator.of(context).pop();
                       },
                     ),
+                  ],
+                ),
+              ),
+              SizedBox(
+                height: 25,
+              ),
+              Padding(
+                padding: const EdgeInsets.only(
+                  left: 10,
+                ),
+                child: Text(
+                  'История заказов',
+                  style: TextStyle(
+                      fontSize: 24,
+                      fontWeight: FontWeight.w600,
+                      color: Color(0xffffffff)
+                  ),
+                ),
+              ),
+              SizedBox(
+                height: 24,
+              ),
+              Container(
+                height: 73,
+                width: double.infinity,
+                color: Color(0xff191A1F),
+                child: Column(
+                  children: [
+                    SizedBox(
+                      height: 12,
+                    ),
                     Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: const [
-                        Text(
-                          'История',
-                          style: TextStyle(
-                            fontSize: 16,
-                            fontWeight: FontWeight.w600,
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Padding(
+                          padding: const EdgeInsets.only(
+                            left: 12,
+                          ),
+                          child: Text('Донер, пицца, вок',
+                            style: TextStyle(
+                                fontSize: 16,
+                                fontWeight: FontWeight.w500,
+                                color: Color(0xffffffff)
+                            ),
+                          ),
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.only(
+                            right: 12,
+                          ),
+                          child: Text('1046,70 р',
+                            style: TextStyle(
+                                fontSize: 16,
+                                fontWeight: FontWeight.w500,
+                                color: Color(0xffFFFFFF)
+                            ),
                           ),
                         ),
                       ],
@@ -45,169 +93,130 @@ class _HistoryOrderState extends State<HistoryOrder> {
                     SizedBox(
                       height: 10,
                     ),
-                    Container(
-                      height: 73,
-                      width: double.infinity,
-                      color: Color(0xff191A1F),
-                      child: Column(
-                        children: [
-                          SizedBox(
-                            height: 12,
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      children: [
+                        Padding(
+                          padding: const EdgeInsets.only(
+                            left: 12,
                           ),
-                          Row(
-                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                            children: [
-                              Padding(
-                                padding: const EdgeInsets.only(
-                                  left: 12,
-                                ),
-                                child: Text('Начисление бонусов',
-                                  style: TextStyle(
-                                      fontSize: 16,
-                                      fontWeight: FontWeight.w500,
-                                      color: Color(0xffffffff)
-                                  ),
-                                ),
+                          child: Container(
+                            padding: EdgeInsets.symmetric(
+                              horizontal: 8,
+                              vertical: 2,
+                            ),
+                            alignment: Alignment.center,
+                            height: 20,
+                            decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(10),
+                              color: Color(0xffff9900),
+                            ),
+                            child: Text('Доставлен',
+                              style: TextStyle(
+                                  fontSize: 12,
+                                  fontWeight: FontWeight.w600,
+                                  color: Color(0xffffffff)
                               ),
-                              Text('+1000',
-                                style: TextStyle(
-                                    fontSize: 16,
-                                    fontWeight: FontWeight.w500,
-                                    color: Color(0xff66BF28)
-                                ),
-                              ),
-                            ],
+                            ),
                           ),
-                          SizedBox(
-                            height: 10,
+                        ),
+                        SizedBox(
+                          width: 10,
+                        ),
+                        Text('21 марта 22:07',
+                          style: TextStyle(
+                              fontWeight: FontWeight.w400,
+                              fontSize: 12,
+                              color: Color(0xff808080)
                           ),
-                          Row(
-                            mainAxisAlignment: MainAxisAlignment.start,
-                            children: [
-                              Padding(
-                                padding: const EdgeInsets.only(
-                                  left: 12,
-                                ),
-                                child: Container(
-                                  padding: EdgeInsets.symmetric(
-                                    horizontal: 8,
-                                    vertical: 2,
-                                  ),
-                                  alignment: Alignment.center,
-                                  height: 20,
-                                  decoration: BoxDecoration(
-                                    borderRadius: BorderRadius.circular(10),
-                                    color: Color(0xff29FF24),
-                                  ),
-                                  child: Text('Еженедельная лотерая',
-                                    style: TextStyle(
-                                        fontSize: 12,
-                                        fontWeight: FontWeight.w600,
-                                        color: Color(0xffffffff)
-                                    ),
-                                  ),
-                                ),
-                              ),
-                              SizedBox(
-                                width: 5,
-                              ),
-                              Text('21 марта',
-                                style: TextStyle(
-                                    fontWeight: FontWeight.w400,
-                                    fontSize: 12,
-                                    color: Color(0xff808080)
-                                ),
-                              ),
-                            ],
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+              SizedBox(
+                height: 4,
+              ),
+              Container(
+                height: 73,
+                width: double.infinity,
+                color: Color(0xff191A1F),
+                child: Column(
+                  children: [
+                    SizedBox(
+                      height: 12,
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Padding(
+                          padding: const EdgeInsets.only(
+                            left: 12,
                           ),
-                        ],
-                      ),
+                          child: Text('Списание бонусов',
+                            style: TextStyle(
+                                fontSize: 16,
+                                fontWeight: FontWeight.w500,
+                                color: Color(0xffffffff)
+                            ),
+                          ),
+                        ),
+                        SizedBox(
+                          width: 127,
+                        ),
+                        Text('-100',
+                          style: TextStyle(
+                              fontSize: 16,
+                              fontWeight: FontWeight.w500,
+                              color: Color(0xffBD3232)
+                          ),
+                        ),
+                      ],
                     ),
                     SizedBox(
-                      height: 4,
+                      height: 10,
                     ),
-                    Container(
-                      height: 73,
-                      width: double.infinity,
-                      color: Color(0xff191A1F),
-                      child: Column(
-                        children: [
-                          SizedBox(
-                            height: 12,
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      children: [
+                        Padding(
+                          padding: const EdgeInsets.only(
+                            left: 12,
                           ),
-                          Row(
-                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                            children: [
-                              Padding(
-                                padding: const EdgeInsets.only(
-                                  left: 12,
-                                ),
-                                child: Text('Списание бонусов',
-                                  style: TextStyle(
-                                      fontSize: 16,
-                                      fontWeight: FontWeight.w500,
-                                      color: Color(0xffffffff)
-                                  ),
-                                ),
+                          child: Container(
+                            decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(10),
+                              color: Color(0xffBD3232),
+                            ),
+                            alignment: Alignment.center,
+                            height: 20,
+                            child: Padding(
+                              padding: const EdgeInsets.symmetric(
+                                vertical: 2,
+                                horizontal: 8,
                               ),
-                              SizedBox(
-                                width: 127,
-                              ),
-                              Text('-100',
+                              child: Text('Оплата покупки',
                                 style: TextStyle(
-                                    fontSize: 16,
-                                    fontWeight: FontWeight.w500,
-                                    color: Color(0xffBD3232)
-                                ),
-                              ),
-                            ],
-                          ),
-                          SizedBox(
-                            height: 10,
-                          ),
-                          Row(
-                            mainAxisAlignment: MainAxisAlignment.start,
-                            children: [
-                              Padding(
-                                padding: const EdgeInsets.only(
-                                  left: 12,
-                                ),
-                                child: Container(
-                                  decoration: BoxDecoration(
-                                    borderRadius: BorderRadius.circular(10),
-                                    color: Color(0xffBD3232),
-                                  ),
-                                  alignment: Alignment.center,
-                                  height: 20,
-                                  child: Padding(
-                                    padding: const EdgeInsets.symmetric(
-                                      vertical: 2,
-                                      horizontal: 8,
-                                    ),
-                                    child: Text('Оплата покупки',
-                                      style: TextStyle(
-                                          fontSize: 12,
-                                          fontWeight: FontWeight.w600,
-                                          color: Color(0xffffffff)
-                                      ),
-                                    ),
-                                  ),
-                                ),
-                              ),
-                              SizedBox(
-                                width: 5,
-                              ),
-                              Text('21 марта',
-                                style: TextStyle(
-                                    fontWeight: FontWeight.w400,
                                     fontSize: 12,
-                                    color: Color(0xff808080)
+                                    fontWeight: FontWeight.w600,
+                                    color: Color(0xffffffff)
                                 ),
                               ),
-                            ],
+                            ),
                           ),
-                        ],
-                      ),
+                        ),
+                        SizedBox(
+                          width: 5,
+                        ),
+                        Text('21 марта',
+                          style: TextStyle(
+                              fontWeight: FontWeight.w400,
+                              fontSize: 12,
+                              color: Color(0xff808080)
+                          ),
+                        ),
+                      ],
                     ),
                   ],
                 ),
